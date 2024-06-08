e49a247aa31ada9705e880f3ae9db4894a88a384
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-08 14:08:05 +0200

    Login User

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -77,6 +77,62 @@ const login = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Email ou mot de passe invalide');
     }
+
+    // Trigger 2FA (Two Factors Authentication) for unknow UserAgent
+    // The User Agent (Insomnia, Postman, Chrome, Safari ...), 
+    // tant qu'il n'est pas stocké en base il va renvoyer un code pour pouvoir rajouter un nouvel agent en base
+    const ua = parser(req.headers['user-agent']);
+    const thisUserAgent = ua.ua;
+    console.log('thisUserAgent', thisUserAgent);
+    const allowedAgent = user.userAgent.includes(thisUserAgent);
+
+    if (!allowedAgent) {
+        // Generate 6 digit code
+        const loginCode = Math.floor(100000 + Math.random() * 900000);
+        console.log('loginCode: ' + loginCode);
+
+        // Encrypt login code before saving to DB
+        const encryptedLoginCode = cryptr.encrypt(loginCode.toString());
+
+        // Delete Token if it exists in DB
+        let userToken = await Token.findOne({ userId: user._id });
+
+        if (userToken) {
+            await userToken.deleteOne();
+        }
+
+        // Save Token To DB
+        await new Token({
+            userId: user._id,
+            lToken: encryptedLoginCode,
+            createdAt: Date.now(),
+            expiresAt: Date.now() + 60 * (60 * 1000) // 1h
+        }).save();
+
+        res.status(400);
+        throw new Error('New Browser or device detected');
+    }
+
+    // Generate token
+    const token = generateToken(user._id);
+
+    if (user && passwordIsCorrect) {
+        // Send HTTP-Only cookie
+        res.cookie('token', token, {
+            path: '/',
+            httpOnly: true,
+            expires: new Date(Date.now() + 1000 * 24 * 60 * 60), // 1 day
+            sameSite: 'none',
+            secure: true
+        });
+
+        const { _id, name, email, phone, bio, photo, role, isVerified } = user;
+
+        res.status(200).json({ _id, name, email, phone, bio, photo, role, isVerified, token });
+    } else {
+        res.status(500);
+        throw new Error('Something went wrong, please try again');
+    }
 });
 
 module.exports = { register, login };
