4e1fcb8cd04f2ddae7acb2db9b69a2667530e65c
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-08 19:33:22 +0200

    Route protected for get your profile

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { register, login, logout, getUser } = require('../controllers/user.controller');
+const { protected, adminOnly, authorOnly } = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
-router.get('/getUser', getUser);
+router.get('/getUser', protected, getUser);
 
 module.exports = router;
